e0d4a3f0ed212c6780edcdaf9ee7ed0c9a915ffc
Yeeun Lee <leeyeah8245@gmail.com>
2026-01-10 19:55:41 +0900

    ✨ (like-api.ts)

--- src/features/like/api/like-api.ts
+++ src/features/like/api/like-api.ts
@@ -1,5 +1,18 @@
 import { fetcher } from '@/shared/lib/axios';
 
-export const postLike = async (id: string) => {
-  const response = fetcher.post(`/api/${id}/like`);
+type TLikeResponse = {
+  success: boolean;
+  isLiked: boolean;
+};
+
+export const postLike = async (id: string): Promise<boolean> => {
+  const response = await fetcher.post<TLikeResponse>(`/api/${id}/like`);
+  const { success, isLiked } = response.data ?? {};
+  return success && isLiked;
+};
+
+export const deleteLike = async (id: string): Promise<boolean> => {
+  const response = await fetcher.delete<TLikeResponse>(`/api/${id}/like`);
+  const { success, isLiked } = response.data ?? {};
+  return success && !isLiked;
 };
