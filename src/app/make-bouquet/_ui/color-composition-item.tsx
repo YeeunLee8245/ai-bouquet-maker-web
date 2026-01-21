43b876e467f8b85f9498a65c9bbf02e6adb3dccc
Yeeun Lee <77772720+YeeunLee8245@users.noreply.github.com>
2026-01-21 13:05:45 +0900

    💄(color-composition-item.tsx)

--- src/app/make-bouquet/_ui/color-composition-item.tsx
+++ src/app/make-bouquet/_ui/color-composition-item.tsx
@@ -31,16 +31,16 @@ export default function ColorCompositionItem({ color, quantity, onPlus, onMinus,
         <button
           type='button'
           onClick={() => onMinus(color)}
-          className='m-1 pl-[4px] pr-[2px]  w-[20px] h-[20px]'>
-          <span className='block w-[14px] h-[2px] bg-gray-200'/>
+          className='m-1 pl-[4px] pr-[2px] w-[20px] h-[20px] rounded hover:bg-gray-100 transition-colors cursor-pointer group'>
+          <span className='block w-[14px] h-[2px] bg-gray-200 group-hover:bg-gray-400 transition-colors'/>
         </button>
         <p className='text-body-md'>{quantity}</p>
         {/* 아이템 개수 증가 버튼 */}
         <button
           type='button'
           onClick={() => onPlus(color)}
-          className='m-1'>
-          <PlusIcon className='pl-[2px] pr-[4px] w-[20px] h-[20px] fill-gray-200' />
+          className='m-1 rounded hover:bg-gray-100 transition-colors cursor-pointer group'>
+          <PlusIcon className='pl-[2px] pr-[4px] w-[20px] h-[20px] fill-gray-200 group-hover:fill-gray-400 transition-colors' />
         </button>
       </div>
     </div>
