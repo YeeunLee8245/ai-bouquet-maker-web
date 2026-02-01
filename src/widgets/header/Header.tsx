c6b7f0da43664bd9086ad60dfb03269e2f4b815e
Yeeun Lee <leeyeah8245@gmail.com>
2026-02-01 10:05:29 +0900

    🚧(header.tsx)

--- src/widgets/header/Header.tsx
+++ src/widgets/header/Header.tsx
@@ -1,11 +1,44 @@
-import DrawerIcon from '@/shared/assets/icons/drawer.svg';
+'use client';
 
-function Header() {
+import DrawerIcon from '@/shared/assets/icons/drawer.svg';
+import { usePathname } from 'next/navigation';
+import LogoIcon from '@/shared/assets/icons/logo.svg';
+import TitleIcon from '@/shared/assets/icons/title.svg';
+import { cn } from '@/shared/utils/styles';
+interface IProps {
+  variant?: 'primary' | 'default';
+}
+
+const containerStyles = (variant: NonNullable<IProps['variant']>) => {
+  return {
+    primary: 'text-[17px] leading-[20px] font-medium tracking-[-0.34px]',
+    default: 'text-[17px] leading-[20px] font-medium tracking-[-0.34px]',
+  }[variant];
+};
+
+const logoWrapperStyles = (variant: NonNullable<IProps['variant']>) => {
+  return {
+    primary: '[&>svg:first-child]:fill-white [&>svg:last-child]:fill-white [&>svg:last-child]:stroke-[0.2]',
+    default: '[&>svg:first-child]:fill-primary-600 [&>svg:last-child]:fill-primary-600 [&>svg:last-child]:stroke-[0.32]',
+  }[variant];
+};
+
+function Header({ variant: variantProp }: IProps) {
+  const pathname = usePathname();
+  const variant = variantProp ?? (pathname === '/main' ? 'primary' : 'default');
+
   return (
     <header className='relative h-[48px] w-full px-4 py-3 flex justify-between items-center z-header'>
-      <span className='text-[17px] leading-[20px] font-medium tracking-[-0.34px]'>꽃다발 레시피</span>
+      <span className={
+        cn(
+          'flex items-center',
+          logoWrapperStyles(variant),
+        )}>
+        <LogoIcon className='ml-[5px] mr-[7.8px] w-[21px] h-[21.8px]' />
+        <TitleIcon />
+      </span>
       <div className='cursor-pointer'>
-        <DrawerIcon />
+        <DrawerIcon className={cn('w-[18px] h-[16px] mx-[3px]', variant === 'primary' ? 'fill-white stroke-white' : 'fill-primary-600 stroke-primary-600')}/>
       </div>
     </header>
   );
