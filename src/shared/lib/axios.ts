f10eebcb3aff603c5bce021ab666aa323a8560e4
Yeeun Lee <leeyeah8245@gmail.com>
2026-03-01 08:51:42 +0900

    ⚡️(axios.ts)

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const fetcher = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_SITE_URL,
   timeout: 10_000,
 });
