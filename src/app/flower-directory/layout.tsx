e1c7433cd63d63df383269e3f549e1ec71e9b439
Yeeun Lee <leeyeah8245@gmail.com>
2026-03-01 23:43:58 +0900

    🐛(app/flower-directory/layout.tsx): Suspense wrap

--- src/app/flower-directory/layout.tsx
+++ src/app/flower-directory/layout.tsx
@@ -1,25 +1,43 @@
 'use client';
 import BottomActionFooter from '@/widgets/footer/bottom-action-footer';
 import { usePathname, useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
 type TProps = {
   children: React.ReactNode;
   modal: React.ReactNode;
 };
 
-export default function FlowerDirectoryLayout({ children, modal }: TProps) {
+/**
+ * 꽃다발 만들기 버튼 Suspense 레이아웃
+ */
+function BottomActionFooterContainer() {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   // 꽃 사전 페이지 또는 꽃 상세 페이지에서 꽃다발 만들기 버튼 표시
   const canCreateBouquet = pathname === '/flower-directory' || searchParams.get('can-create-bouquet') === 'true';
 
   return (
-    <div className='relative h-full flex flex-col'>
-      <div className='relative flex flex-1 overflow-y-auto'>
-        {children}
-        {modal}
-      </div>
+    <>
       {canCreateBouquet && <BottomActionFooter title='꽃다발 만들기' />}
-    </div>
+    </>
+  );
+}
+
+/**
+ * 꽃 사전 레이아웃
+ */
+export default function FlowerDirectoryLayout({ children, modal }: TProps) {
+
+  return (
+    <Suspense fallback={null}>
+      <div className='relative h-full flex flex-col'>
+        <div className='relative flex flex-1 overflow-y-auto'>
+          {children}
+          {modal}
+        </div>
+        <BottomActionFooterContainer />
+      </div>
+    </Suspense>
   );
 }
