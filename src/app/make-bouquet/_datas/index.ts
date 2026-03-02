878a1de7feba5450dc73f155a0aadbc587c2f235
Yeeun Lee <leeyeah8245@gmail.com>
2026-03-02 12:52:33 +0900

    build error

--- src/app/make-bouquet/_datas/index.ts
+++ src/app/make-bouquet/_datas/index.ts
@@ -1,4 +1,4 @@
-import { TFlowerCompositionItem, TMakeBouquetInfoData } from '../_types';
+import { TMakeBouquetInfoData } from '../_types';
 
 export const MAKE_BOUQUET_INFO_DATAS: TMakeBouquetInfoData[] = [
   {
@@ -20,44 +20,5 @@ export const MAKE_BOUQUET_INFO_DATAS: TMakeBouquetInfoData[] = [
     title: '전달 메시지',
     placeholder: '전달할 메시지를 입력해주세요.',
     isRequired: false,
-  },
-];
-
-export const FLOWER_COMPOSITION_ITEMS: TFlowerCompositionItem[] = [
-  {
-    id: 1,
-    name: '리시안셔스',
-    keywords: ['행복', '감사', '축하'],
-    imageUrl: '/temp_geobera.png',
-    colorAndQuantities: [
-      {
-        // 주황
-        color: '#FF8D3E',
-        quantity: 1,
-      },
-      {
-        // 초록
-        color: '#00C240',
-        quantity: 1,
-      },
-    ],
-  },
-  {
-    id: 2,
-    name: '거베라',
-    keywords: ['행복', '감사', '축하'],
-    imageUrl: '/temp_geobera.png',
-    colorAndQuantities: [
-      {
-        // 주황
-        color: '#FF8D3E',
-        quantity: 1,
-      },
-      {
-        // 초록
-        color: '#00C240',
-        quantity: 1,
-      },
-    ],
   },
 ];
