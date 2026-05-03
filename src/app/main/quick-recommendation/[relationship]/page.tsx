07c073ce596fab3b15b64de2fced232bcb24d976
Yeeun Lee <leeyeah8245@gmail.com>
2026-05-03 11:24:25 +0900

    ✨: 빠른 추천 화면 반응형

--- src/app/main/quick-recommendation/[relationship]/page.tsx
+++ src/app/main/quick-recommendation/[relationship]/page.tsx
@@ -26,7 +26,7 @@ function QuickRecommendationPage() {
 
   return (
     <div>
-      <div className='px-5 pt-4 pb-2'>
+      <div className='px-5 pt-4 pb-2 tablet:pl-6 tablet:pr-4'>
         <div className='text-title-lg'>
           {`${QUICK_RECOMMENDATION_DATA_MAP[relationship]}에게 전할 꽃`}
         </div>
@@ -34,7 +34,7 @@ function QuickRecommendationPage() {
           어떤 상황에 전달할 꽃인가요?
         </div>
       </div>
-      <div className='grid grid-cols-2 gap-4 px-4 pt-4 pb-8'>
+      <div className='grid grid-cols-2 tablet:grid-cols-4 gap-4 px-4 tablet:px-6 pt-4 pb-8'>
         {isPending
           ? Array.from({ length: 4 }).map((_, i) => <OccasionItemSkeleton key={i} />)
           : occasions?.map((item) => (
