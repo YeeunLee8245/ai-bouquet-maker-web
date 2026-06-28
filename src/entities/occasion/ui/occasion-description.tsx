286bca4d3dc08494f9406c3dd9cb2cb65ddb96bf
Yeeun Lee <leeyeah8245@gmail.com>
2026-06-28 11:59:21 +0900

    🐛: 빠른 추천 이미지 size

--- src/entities/occasion/ui/occasion-description.tsx
+++ src/entities/occasion/ui/occasion-description.tsx
@@ -11,7 +11,7 @@ function OccasionDescription({ type, title, description }: IProps) {
 
   return <div className='h-[180px] pt-2 px-4 tablet:px-6 pc:px-8 pb-4 flex flex-col items-center gap-[2px]'>
     <div className='flex-1 content-center'>
-      <OccasionImage type={type} className='w-auto h-auto' priority />
+      <OccasionImage type={type} priority />
     </div>
     <div className='text-title-lg'>{title}</div>
     <div className='text-body-md text-gray-400 text-center whitespace-pre-wrap'>{description}</div>
